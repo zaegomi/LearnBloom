02f2e14e06198783b843908444823260ae3b4efe
zaegomi <160190635+zaegomi@users.noreply.github.com>
2025-06-08 06:55:39 +0000

    a

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.options('*', (req, res) => {
   res.sendStatus(200);
 });
 
-// Add CORS headers to all responses
+// Add CORS headers to all responsess
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
